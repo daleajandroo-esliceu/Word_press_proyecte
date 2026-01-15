2044155a5e6383a93c5021d5021c1cb9a8088b77
Daniel Alejandro Ortiz <dalejandroo@esliceu.net>
2026-01-15 17:39:51 +0100

    video 12

--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.rtl.min.css" integrity="sha384-CfCrinSRH2IR6a4e6fy2q6ioOX7O6Mtm1L9vRvFZ1trBncWmMePhzvafv7oIcWiW" crossorigin="anonymous">
     <?php wp_head();?>
 
     <title>Mi página de inicio</title>
