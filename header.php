5b3d844c4e788e28e9f14944889de1732c110f53
Daniel Alejandro Ortiz <dalejandroo@esliceu.net>
2026-01-22 16:18:44 +0100

    video 13

--- header.php
+++ header.php
@@ -9,4 +9,27 @@
 </head>
 <body <?php body_class()?>>
 
-<h1>Menu</h1>
+<div class="cabecer">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+  <a class="navbar-brand" href="/">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="Blog">Blog<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Acerca-de-nosotros">Acerca-de</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Contacto">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
